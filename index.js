9e9ee1b6b88780591a8d9980b5d13ee90f485271
Marcus Thelin <marcusthelin97@gmail.com>
2018-06-21 19:06:03 +0200

    Change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('HELLO THERE WORLD!');
+  res.send('Emil suger på Rocket League');
 });
 
 app.listen(port, () => console.log('Started!'));
