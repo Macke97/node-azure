2ee7886be677d5aac93eee0bff2fbe92f1dacb0d
Marcus Thelin <marcusthelin97@gmail.com>
2018-06-21 19:17:42 +0200

    Added body parser

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('Emil suger på Rocket League');
+  req.body.test = {message: 'HEJSAN'};
+  res.json(req.body);
 });
 
 app.listen(port, () => console.log('Started!'));
